a12318ab422b7edd20721d22afda1821017fb999
owen-m1ller <omiller@ucsd.edu>
2026-02-15 22:22:28 -0800

    add new data folders and modify script to install Jenny's data

--- scripts/data_install.sh
+++ scripts/data_install.sh
@@ -24,3 +24,35 @@ for item in "${video_names[@]}"; do
     ffmpeg -i ../$video_path/$item ../$frames_path/${item%.*}/frame_%04d.png
 done
 
+# install image subfolders
+
+
+image_path="data/images/blue"
+
+for i in {1..20}; do
+    curl https://pub-753cbd3506414678a87262edde600418.r2.dev/$image_path/$i.jpg -o ../$image_path/$i.jpg
+done
+
+image_path="data/images/few"
+
+for i in {1..10}; do
+    curl https://pub-753cbd3506414678a87262edde600418.r2.dev/$image_path/$i.jpg -o ../$image_path/$i.jpg
+done
+
+image_path="data/images/many"
+
+for i in {1..10}; do
+    curl https://pub-753cbd3506414678a87262edde600418.r2.dev/$image_path/$i.jpg -o ../$image_path/$i.jpg
+done
+
+image_path="data/images/red"
+
+for i in {1..10}; do
+    curl https://pub-753cbd3506414678a87262edde600418.r2.dev/$image_path/$i.jpg -o ../$image_path/$i.jpg
+done
+
+image_names=("day_ocean.jpg" "nion.jpg" "ocean_day.jpg" "ocean_sunset.jpg" "red_leaf.jpg")
+image_path="data/images/images"
+for item in "${image_names[@]}"; do
+    curl https://pub-753cbd3506414678a87262edde600418.r2.dev/$image_path/$item -o ../$image_path/$item
+done
